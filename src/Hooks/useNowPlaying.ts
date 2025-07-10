38de6bf904a655b310c24036e634dd4806057780
Anish Raj <anishrajdmk@gmail.com>
2025-07-10 12:59:20 +0530

    Removed the .env from git

--- src/Hooks/useNowPlaying.ts
+++ src/Hooks/useNowPlaying.ts
@@ -4,7 +4,7 @@ import {fetchNowPlaying, type Movie} from "../api/apiFunctions.ts";
 export const useNowPlaying=()=>{
     return useQuery <Movie[], Error>({
         queryKey:['now-playing'],
-        queryFn:()=>fetchNowPlaying(1),
+        queryFn:()=>fetchNowPlaying(),
         staleTime: 10*60*1000,
         gcTime:5*60*1000,
     });
